853d5c6f829f8f5fc2fa5ba4e3c6f03239e43773
Auberon López <auberon.lopez@greenriver.edu>
2025-11-25 12:23:57 -0800

    Add compilation / running directions and fix port type

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,16 +1,19 @@
 import java.io.IOException;
 
+// To recompile files and run client:
+// javac src/*.java && java -cp src ChatClient
+
 public class ChatClient {
 
     public static void main(String[] args) {
-        
+       
     }
 
     public static void readFromFile(String filename) throws IOException{
 
     }
 
-    public static void readFromSocket(String host, String port) throws IOException {
+    public static void readFromSocket(String host, int port) throws IOException {
         
     }
 }
--- src/OutServer.java
+++ src/OutServer.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
 
+// To recompile files and run server:
+// javac src/*.java && java -cp src OutServer
 public class OutServer {
+    
     public static void main(String[] args) {
-
+        
     }
 
     public void writeToSocket(int port) throws IOException {
